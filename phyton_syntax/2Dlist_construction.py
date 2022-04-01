40bb76a677914f5fda99b6c11bfd12e496a91a8c
sann <55998816+kimtks456@users.noreply.github.com>
2022-04-01 20:09:32 +0900

    Update 2Dlist_construction.py

--- phyton_syntax/2Dlist_construction.py
+++ phyton_syntax/2Dlist_construction.py
@@ -8,3 +8,8 @@ arr
 
 # * num 이렇게 생성하면, 한곳만 바꿔도 해당 열 다 반영됨. 
 # list comprehension 이용할것.
+
+# Useful to know: when you create a list of a specific size, 
+# for instance using the expression [ None ] * 1000 or [0] * 1000,
+# the list will be created without any extra space,
+# so the capacity will be equal to the size.
