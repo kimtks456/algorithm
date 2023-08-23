b9065171c427a87262fafb080c63e3824e7ea2a7
san <55998816+kimtks456@users.noreply.github.com>
2023-08-24 02:27:46 +0900

    greedy - 수정

--- Baekjun_Online_Judge/18230_2xN_예쁜_타일링.py
+++ Baekjun_Online_Judge/18230_2xN_예쁜_타일링.py
@@ -1,28 +1,26 @@
 n, a, b = map(int, input().split())
-weight1 = list(map(int, input().split()))
-weight2 = list(map(int, input().split()))
+weight1 = sorted(list(map(int, input().split())))
+weight2 = sorted(list(map(int, input().split())))
 answer = 0
 
-tile_b_count = n // 2
-tile_a_count = n % 2
-used_count = 0
+if n % 2 != 0:
+    n -= 1
+    answer += weight1.pop()
 
-while used_count <
-
-weight1.sort()
-while len(weight1) > 1:
-    print(weight1)
-    if n % 2 != 0 and len(weight1) == 2:
-        break
-
-    w1, w2 = weight1.pop(), weight1.pop()
-    weight2.append(w1 + w2)
-
-weight2.sort(reverse=True)
-tile_b_count = n // 2
-
-for i in range(tile_b_count):
-    answer += weight2[i]
-answer += 0 if n % 2 == 0 else weight1[-1]
+while n:
+    n -= 2
+    if len(weight1) >= 2:
+        w1, w2 = weight1.pop(), weight1.pop()
+        if len(weight2) > 0:
+            if w1 + w2 > weight2[-1]:
+                answer += w1 + w2
+            else:
+                answer += weight2.pop()
+                weight1.append(w2)
+                weight1.append(w1)
+        else:
+            answer += w1 + w2
+    else:
+        answer += weight2.pop()
 
 print(answer)
