fea19d14889accc4a3bc39cda32f84c0484484cb
san <55998816+kimtks456@users.noreply.github.com>
2023-09-08 03:10:13 +0900

    DP

---
+++ Baekjun_Online_Judge/DP_9461_파도반_수열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+p = [0] * 101
+p[1], p[2], p[3], p[4], p[5] = 1, 1, 1, 2, 2
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    for i in range(6, n + 1):
+        p[i] = p[i - 1] + p[i - 5]
+    print(p[n])
+
+
