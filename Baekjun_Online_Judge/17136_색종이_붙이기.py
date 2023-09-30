3479bcdd38604a35bc98584cf92abe79980a2511
san <55998816+kimtks456@users.noreply.github.com>
2023-09-30 21:54:03 +0900

    BT: 구현 어려운 문제, Python3는 시간초과. PyPy3 통과

--- Baekjun_Online_Judge/17136_색종이_붙이기.py
+++ Baekjun_Online_Judge/17136_색종이_붙이기.py
@@ -23,76 +23,68 @@ def find_max_paper(maps, start):
             return d
 
 
-def solve(maps, papers):
-    # 1이 없다면 종료
-    flag = False
-    for i in range(10):
-        for j in range(10):
+# 1이 없다면 True
+def check_all_zero():
+    flag = True
+    i = 0
+    while flag and i in range(10):
+        j = 0
+        while flag and j in range(10):
             if maps[i][j] == 1:
-                flag = True
-    if flag is False:
-        return 25 - sum(papers), papers
+                flag = False
+            j += 1
+        i += 1
+    return flag
 
-    # 1이 있는데 paper를 다 썼다면 종료
-    if papers == [0] * 5 and flag is True:
-        return 26, papers
 
-    global result
-    result_papers = []
-    for i in range(10):
-        for j in range(10):
-            if maps[i][j] == 1:
-                d = find_max_paper(maps, (i, j))
-                # print("now i, j, d : ", i, j, d)
-                # print("papers : ", papers)
-                # for row in maps:
-                #     print(row)
-                # print()
-                while d > 0:
-                    # 해당 paper가 없으면 다음 색종이로 넘어가기
-                    if papers[d - 1] == 0:
-                        if d == 1:
-                            return 26, result_papers
-                        d -= 1
-                        continue
+def solve(count):
+    global answer
+    flag = True
 
-                    # 색종이 붙이기
-                    for ii in range(i, i + d):
-                        for jj in range(j, j + d):
-                            maps[ii][jj] = 0
-                    papers[d - 1] -= 1
+    i = 0
+    while flag and i in range(10):
+        j = 0
+        while flag and j in range(10):
+            if maps[i][j] == 0:
+                j += 1
+                continue
 
-                    # 1이 없다면 종료
-                    flag2 = False
-                    for ii in range(10):
-                        for jj in range(10):
-                            if maps[ii][jj] == 1:
-                                flag2 = True
-                    if flag2 is False:
-                        # print("no ones")
-                        # for row in maps:
-                        #     print(row)
-                        # print(papers)
-                        return 25 - sum(papers), papers
-
-                    temp_result1, result_papers = solve(maps, papers)
-
-                    # 색종이 다시 떼기
-                    for ii in range(i, i + d):
-                        for jj in range(j, j + d):
-                            maps[ii][jj] = 1
-                    papers[d - 1] += 1
-
-                    result = min(result, temp_result1)
+            d = find_max_paper(maps, (i, j))
+            # print(i, j, d, papers)
+            # for row in maps:
+            #     print(row)
+            # print()
+            while d > 0:
+                if papers[d - 1] == 0:
                     d -= 1
-    # print("final : ", result, result_papers)
-    return result, result_papers
+                    continue
+
+                papers[d - 1] -= 1
+                for ii in range(i, i + d):
+                    for jj in range(j, j + d):
+                        maps[ii][jj] = 0
+
+                solve(count + 1)
+
+                papers[d - 1] += 1
+                for ii in range(i, i + d):
+                    for jj in range(j, j + d):
+                        maps[ii][jj] = 1
+
+                d -= 1
+            flag = False
+        i += 1
+
+    if check_all_zero():
+        answer = min(answer, count)
 
 
 maps = [list(map(int, sys.stdin.readline().split())) for _ in range(10)]
 
-result = 0
-answer, papers = solve(maps, [5] * 5)
+answer, papers = 26, [5] * 5
+
+solve(0)
+
 if answer == 26:
     print(-1)
 else:
