0fc3e0b4523ed777d1c65d17606a2770e4851172
san <55998816+kimtks456@users.noreply.github.com>
2023-10-02 02:06:31 +0900

    permutation 다 저장할 경우, 메모리 보기

--- Baekjun_Online_Judge/17281_⚾.py
+++ Baekjun_Online_Judge/17281_⚾.py
@@ -1,1 +1,65 @@
-32 +
+import sys
+
+
+def permutation(arr, r):
+    result = []
+    visited = [False] * len(arr)
+
+    def dfs(case):
+        if len(case) == r:
+            result.append(case[:])
+            return
+
+        for i, v in enumerate(arr):
+            if visited[i]:
+                continue
+
+            case.append(v)
+            visited[i] = True
+            dfs(case)
+
+            case.pop()
+            visited[i] = False
+
+    dfs([])
+    return result
+
+
+def solve():
+    score = 0
+
+    for case in cases:
+        temp_score = 0
+        case = case[0:3] + [0] + case[3:]
+        # if case == [1, 2, 3, 0, 4, 5, 6, 7, 8]:
+        #     print("now")
+        #     break
+        hitter_idx = 0
+        for inning in innings:
+            out = 0
+            base = [0, 0, 0, 0]
+            while out < 3:
+                hitter_seq = case[hitter_idx]
+                hit = inning[hitter_seq]
+                if hit == 0:
+                    out += 1
+                else:
+                    temp_score += sum(base[4 - hit:]) + (hit == 4)
+                    base = [0] * (hit - 1 + (hit == 4)) + [1] * (hit < 4) + base[:4 - hit]
+                hitter_idx = (hitter_idx + 1) % 9
+                # print("inning : ", inning)
+                # print(hitter_idx, temp_score, base, out)
+                # print()
+
+        score = max(score, temp_score)
+
+    return score
+
+
+n = int(sys.stdin.readline())
+innings = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+cases = permutation([i for i in range(1, 9)], 8)
+# cases = [[1, 2, 3, 4, 5, 6, 7, 8]]
+answer = solve()
+
+print(answer)
