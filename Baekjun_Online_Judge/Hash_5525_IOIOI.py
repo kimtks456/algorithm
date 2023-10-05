508407c603c50e1d372b798c2373630ff0f4e586
san <55998816+kimtks456@users.noreply.github.com>
2023-10-05 20:43:52 +0900

    SIM: 시간 초과 때문에 O(N) 으로 변경

--- Baekjun_Online_Judge/Hash_5525_IOIOI.py
+++ Baekjun_Online_Judge/Hash_5525_IOIOI.py
@@ -2,12 +2,38 @@ n = int(input())
 m = int(input())
 s = input().rstrip()
 
-pn = ""
-while len(pn) < 2 * n:
-    pn += "IO"
-pn += "I"
+pn = "IO" * n + "I"
 
-# # dictionary 풀이법
+i, answer = 0, 0
+temp_count = 0
+
+while i < m:
+    if temp_count == n:
+        answer += 1
+        temp_count -= 1
+    if i < m - 2 and ''.join([s[i], s[i + 1], s[i + 2]]) == "IOI":
+        temp_count += 1
+        i += 2
+        continue
+    else:
+        i += 1
+        temp_count = 0
+
+print(answer)
+
+# # BRTF 풀이법 -> 부분점수만 받음
+# answer = 0
+# for i in range(m - len(pn) + 1):
+#     flag = True
+#     for j in range(len(pn)):
+#         flag = flag and (pn[j] == s[i + j])
+#     if flag:
+#         answer += 1
+#
+# print(answer)
+
+
+# # dictionary 풀이법 -> 시간초과
 # count = dict()
 # for i in range(m - len(pn) + 1):
 #     now = ""
@@ -20,16 +46,7 @@ pn += "I"
 #
 # print(count[pn])
 
-# BRTF 풀이법
-answer = 0
-for i in range(m - len(pn) + 1):
-    flag = True
-    for j in range(len(pn)):
-        flag = flag and (pn[j] == s[i + j])
-    if flag:
-        answer += 1
 
-print(answer)
 
 """
 
