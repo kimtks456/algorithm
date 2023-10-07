74f7d14cf52bf784d1ab7d6399a11ed6c365d190
san <55998816+kimtks456@users.noreply.github.com>
2023-10-07 16:17:06 +0900

    refactor

--- Baekjun_Online_Judge/DP_2579_계단_오르기.py
+++ Baekjun_Online_Judge/DP_2579_계단_오르기.py
@@ -9,8 +9,8 @@ dp = [0] * n
 if n < 3:
     print(sum(stair))
 else:
+    dp[0], dp[1] = stair[0], stair[0] + stair[1]
     for i in range(2, n):
-        dp[0], dp[1] = stair[0], stair[0] + stair[1]
         dp[i] = max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i])
     print(dp[n - 1])
 
