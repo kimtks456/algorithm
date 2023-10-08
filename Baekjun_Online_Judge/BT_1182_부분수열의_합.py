6d2e7d4ded764a7d8e32dbe16fe41775b9b5239f
san <55998816+kimtks456@users.noreply.github.com>
2023-10-08 12:15:00 +0900

    BT: 기본

---
+++ Baekjun_Online_Judge/BT_1182_부분수열의_합.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+
+def solve2(start, temp):
+    if start == N:
+        if sum(temp) == S and len(temp) > 0:
+            return 1
+        return 0
+
+    return solve2(start + 1, temp[:] + [nums[start]]) + solve2(start + 1, temp[:])
+
+# def solve(start, temp):
+#     if start < N:
+#         print(start, temp)
+#     if start == N:
+#         if temp == S:
+#             print(temp)
+#             return 1
+#         return 0
+#
+#     return solve(start + 1, temp + nums[start]) + solve(start + 1, temp)
+
+
+N, S = map(int, input().split())
+nums = list(map(int, input().split()))
+answer = 0
+
+# print(solve(0, 0))
+print(solve2(0, []))
