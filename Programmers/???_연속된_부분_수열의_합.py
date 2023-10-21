0ac4eec033daaa3fb1fe67b1ded71a773c5969f3
san <55998816+kimtks456@users.noreply.github.com>
2023-10-21 23:55:04 +0900

    basic: 시간 초과

--- Programmers/???_연속된_부분_수열의_합.py
+++ Programmers/???_연속된_부분_수열의_합.py
@@ -5,25 +5,17 @@ def solution(sequence, k):
     for i in range(1, n):
         presum[i] = presum[i - 1] + sequence[i]
 
-    print(presum)
-    l, r = 0, n - 1
-    while 0 <= l <= r < n:
-        temp = presum[r] - presum[l] + sequence[l]
+    for i in range(n):
+        for j in range(i + 1):
 
-        if temp == k:
-            if answer:
-                ll, rr = answer[0], answer[1]
-                if (rr - ll > r - l) or (rr - ll == r - l and ll > l):
-                    answer[0], answer[1] = l, r
-            else:
-                answer.append(l)
-                answer.append(r)
-            l += 1
-            r -= 1
-
-        elif temp < k:
-            l += 1
-        else:
-            r -= 1
+            temp = presum[i] - presum[j] + sequence[j]
+            if temp == k:
+                if answer:
+                    ll, rr = answer[0], answer[1]
+                    if (rr - ll > i - j) or (rr - ll == i - j and ll > j):
+                        answer = [j, i]
+                else:
+                    answer.append(j)
+                    answer.append(i)
 
     return answer
