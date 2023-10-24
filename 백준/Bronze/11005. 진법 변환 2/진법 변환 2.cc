2ada2f927b51fe4385b4c0452e496678cf4830c1
san <55998816+kimtks456@users.noreply.github.com>
2023-10-25 04:06:05 +0900

    중요: char 배열 초기화 반드시

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -1,30 +1,39 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 using namespace std;
+char isOverTen(int);
 
-int main()
-{
-    int n, b;
+int main() {
+    char res[101]{ 0, };
+    string answer2{ "" };
+    int n, b, i{ 0 };
     cin >> n >> b;
 
-    string b_num;
-    while (n != 0)
-    {
-        int tmp = n % b;
-        if (tmp > 9)
-        {
-            tmp = tmp - 10 + 'A';
-            b_num += tmp;
-        }
-        else
-        {
-            b_num += ('0' + tmp);
-        }
+    while (n > 0) {
+        res[i] = isOverTen(n % b);
+        answer2 += isOverTen(n % b);
         n /= b;
+        i++;
     }
-    reverse(b_num.begin(), b_num.end());
 
-    cout << b_num << '\n';
+    // 1. char[] -> string 만들어서 뒤집기 : char 배열 끝을 알리는 \0도 포함되므로 답 처리가 안됨.
+    // \0이 포함되는게 아니라 초기화를 안해서 쓰레기 값이 들어가 있었기에 그게 포함됐음. -> 초기화해서 해결!
+    string answer(res);
+    reverse(answer.begin(), answer.end());
+
+    // 2. 처음부터 string에다가 string += char 해서 뒤집기 : 이것도 정답임.
+    reverse(answer2.begin(), answer2.end());
+
+    // cout << answer << '.' << answer2 << '.' << answer.compare(answer2) << '\n';
+    // cout << answer.size() << " : " << answer2.size() << '\n';
+    // cout << answer.substr(0, answer.size());
+    cout << answer;
 
     return 0;
+}
+
+char isOverTen(int a) {
+    if (a > 9) return 'A' + (a - 10);
+    else return a + '0';
 }
