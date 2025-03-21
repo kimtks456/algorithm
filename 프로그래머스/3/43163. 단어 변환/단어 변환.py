88160324910948ed07567b7a4cfd0b4ad2298993
San <55998816+kimtks456@users.noreply.github.com>
2025-03-22 03:33:02 +0900

    [level 3] Title: 단어 변환, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+
+def findNext(now, words):
+    target = []
+    for word in words:
+        if sum(i==j for i, j in zip(now, word)) == len(now) - 1:
+            target.append(word)
+    return target
+    
+def solution(begin, target, words):
+    answer = 0
+    q = deque()
+    q.append((begin, 0))
+    visited = set()
+    visited.add(begin)
+
+    while q:
+        now, trial = q.popleft()
+        
+        if now == target:
+            return trial
+        
+        cand = findNext(now, words)
+        for word in cand:
+            if word not in visited:
+                q.append((word, trial+1))
+                visited.add(word)
+
+    return answer
+
+# "hit"   "bak"   ["hot", "dot", "dog", "lot", "log", "bak"]
