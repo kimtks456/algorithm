499564702fdec8e24493404178281feb8c6e35e4
San <55998816+kimtks456@users.noreply.github.com>
2025-10-17 21:39:15 +0900

    [level 3] Title: 단어 변환, Time: 0.58 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,32 +1,84 @@
 from collections import deque
 
-
-def findNext(now, words):
-    target = []
-    for word in words:
-        if sum(i==j for i, j in zip(now, word)) == len(now) - 1:
-            target.append(word)
-    return target
-    
 def solution(begin, target, words):
-    answer = 0
-    q = deque()
-    q.append((begin, 0))
-    visited = set()
-    visited.add(begin)
-
-    while q:
-        now, trial = q.popleft()
+    
+    def next_words(target):
+        result = []
+        for to in words:
+            if to == target:
+                continue
+            
+            diffCnt = 0
+            for i in range(len(target)):
+                if target[i] != to[i]:
+                    diffCnt += 1
+            
+            if diffCnt == 1:
+                result.append(to)
+                
+        return result
+    
+    def bfs(start):
+        visited = set([start])
+        que = deque()
+        que.append((start, 0))
         
-        if now == target:
-            return trial
-        
-        cand = findNext(now, words)
-        for word in cand:
-            if word not in visited:
-                q.append((word, trial+1))
-                visited.add(word)
+        while que:
+            now_word, path = que.popleft()
+            
+            if now_word == target:
+                return path
+            
+            for word in next_words(now_word):
+                if word not in visited:
+                    que.append((word, path + 1))
+                    visited.add(word)
+            
+        return 0
+    
+    return bfs(begin)
 
-    return answer
 
-# "hit"   "bak"   ["hot", "dot", "dog", "lot", "log", "bak"]
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+# 
+# 
+# def findNext(now, words):
+#     target = []
+#     for word in words:
+#         if sum(i==j for i, j in zip(now, word)) == len(now) - 1:
+#             target.append(word)
+#     return target
+#     
+# def solution(begin, target, words):
+#     answer = 0
+#     q = deque()
+#     q.append((begin, 0))
+#     visited = set()
+#     visited.add(begin)
+# 
+#     while q:
+#         now, trial = q.popleft()
+#         
+#         if now == target:
+#             return trial
+#         
+#         cand = findNext(now, words)
+#         for word in cand:
+#             if word not in visited:
+#                 q.append((word, trial+1))
+#                 visited.add(word)
+# 
+#     return answer
+# 
+# # "hit"   "bak"   ["hot", "dot", "dog", "lot", "log", "bak"]
