1e2366b5f080a4e109d523834eba58f61dcddffb
San <55998816+kimtks456@users.noreply.github.com>
2025-10-19 17:21:31 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 9.42 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,74 @@
+def solution(n, computers):
+    visited = [False] * n
+    answer = 0
+    
+    def dfs(start):
+        stack = [start]
+        
+        while stack:
+            node = stack.pop()
+            for next_node, isConnected in enumerate(computers[node]):
+                if next_node != node and isConnected and not visited[next_node]:
+                    visited[next_node] = True
+                    stack.append(next_node)
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            dfs(i)
+            answer += 1
+    return answer
+
+# # def dfs(start, visited, com):
+# #     visited[start] = 1
+# #     for i, neighbor in enumerate(com[start]):
+# #         if i != start and visited[i] == 0 and neighbor == 1:
+# #             dfs(i, visited, com)
+#         
+# #     return visited
+# 
+# # def solution(n, computers):
+# #     answer = 1
+# #     visited = [0] * n
+# #     start = 0
+#     
+# #     while True:
+# #         visited = dfs(start, visited, computers)
+# #         if False in visited:
+# #             start = visited.index(False)
+# #             answer += 1
+# #         else:
+# #             break
+# #     return answer
+# 
+# from collections import deque
+# 
+# 
+# def solution(n, computers):
+#     answer = n
+#     visited = [0] * n
+# 
+#     q = deque([0])
+#     visited[0] = 1
+# 
+#     while q:
+#         now = q.popleft()
+#         print("now : ", now)
+# 
+#         for i in range(n):
+#             if now == i:
+#                 continue
+# 
+#             if visited[i] != 1:
+#                 q.append(i)
+# 
+#             if visited[i] != 1 and computers[now][i] == 1:
+#                 print("visit : " , visited[i])
+#                 print("i " ,i)
+#                 answer -= 1
+# 
+#         visited[now] = 1
+# 
+#     return answer
+# 
