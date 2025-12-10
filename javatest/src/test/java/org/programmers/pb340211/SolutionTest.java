894802dbd9f4113f1a8ae3bda4e15a435a05acf8
kimtks456 <kimtks456@gmail.com>
2025-12-11 01:35:00 +0900

    test: 프로그래머스 - [PCCP 기출문제] 3번 / 충돌위험 찾기

---
+++ javatest/src/test/java/org/programmers/pb340211/SolutionTest.java
@@ -0,0 +1,33 @@
+package org.programmers.pb340211;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    record TestCase(int[][] points, int[][] routes, int expected) {}
+
+    @Test
+    void solution() {
+        // given
+        Stream<TestCase> tcs = Stream.of(
+                new TestCase(new int[][] {{3,2},{6,4},{4,7},{1,4}},
+                    new int[][] {{4,2},{1,3},{2,4}}, 1),
+                new TestCase(new int[][] {{3,2},{6,4},{4,7},{1,4}},
+                        new int[][] {{4,2},{1,3},{4,2},{4,3}}, 9),
+                new TestCase(new int[][] {{2,2},{2,3},{2,7},{6,6},{5,2}},
+                        new int[][] {{2,3,4,5},{1,3,4,5}}, 0)
+                );
+
+        Solution solution = new Solution();
+
+        // when & then
+        tcs.forEach(tc -> {
+            int result = solution.solution(tc.points, tc.routes);
+            assertThat(result).isEqualTo(tc.expected);
+        });
+    }
+}
