4d7b7ed98274514dd3c527939ddd412988b0046e
sani <55998816+kimtks456@users.noreply.github.com>
2026-02-13 00:51:16 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 20.88 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -2,94 +2,18 @@ import java.util.*;
 
 
 class Solution {
-    
     public String[] solution(String[] expressions) {
-        // "X"가 포함된 수식
-        List<String> targets = new ArrayList<>();
-        // 사용가능한 진법 Set
-        Set<Integer> posBases = new HashSet<>();
+        String[] answer = {};
         
-        // 최소 진법 찾기
-        int minBase = Integer.MIN_VALUE;
-        for (String expression : expressions) {
-            int base = getMinBase(expression.split("[ +\\-=]+"));
-            minBase = Math.max(minBase, base);
+        for (int i = 2; i < 10; i++) {
+            sout(String.valueOf(isBase(expressions[0], i)) + i);
         }
         
-        // 사용가능한 진법 조회
-        for (int i = minBase + 1; i < 10; i++) {
-            
-            boolean isPossible = true;
-            
-            for (int j = 0; j < expressions.length; j++) {
-           		String[] terms = expressions[j].split("[ +\\-=]+");
-                
-                // 한번만 실행
-                if (terms[2].contains("X")) {
-                    if (i == minBase + 1) {
-                    	targets.add(expressions[j]);
-                    }
-                    continue;
-                }
-                
-                if (!isBase(expressions[j], terms, i)) {
-                    isPossible = false;
-                }
-            }
-            
-            if (isPossible) {
-                posBases.add(i);
-            }
-        }
-        
-        // "X" 치환
-        List<String> answer = new ArrayList<>();
-        
-        for (String target : targets) {
-            String[] terms = target.split("[ +\\-=]+");
-            int targetBase = 0;
-            int result = Integer.MAX_VALUE;
-            int calc = Integer.MAX_VALUE;
-            boolean flag = false;
-            for (Integer base : posBases) {
-            	// 진법 적용한 결과값
-            	calc = toBase(calculate(target, terms, base), base);
-                
-                
-                if (targetBase == 0) {
-                    result = calc;
-                }
-                
-                if (result != Integer.MAX_VALUE && result != calc) {
-                    answer.add(target.replace("X", "?"));
-                    flag = true;
-                    break;
-                }
-                
-                result = calc;
-                targetBase = base;
-            }
-            
-            if (!flag) {
-                answer.add(target.replace("X", String.valueOf(calc)));
-            }
-        }
-        
-        return answer.toArray(String[]::new);
+        return answer;
     }
     
-    private int getMinBase(String[] terms) {
-        return Arrays.stream(terms)
-            .flatMapToInt(String::chars)
-            .filter(c -> Character.isDigit(c)) // isDigit은 실제 숫자인 character 또는 숫자 char의 ASCII코드값을 받음
-            .map(c -> c - '0')
-            .max()
-            .orElse(Integer.MAX_VALUE);
-    }
-    
-    private boolean isBase(String exp, String[] terms, int base) {
-        
-        int calc = calculate(exp, terms, base);
+    private boolean isBase(String exp, int base) {
+        String[] terms = parseExp(exp);
         String[] first = terms[0].split("");
         String[] second = terms[1].split("");
         String[] result = terms[2].split("");
@@ -98,41 +22,35 @@ class Solution {
             return false;
         }
         
-        return calc == toDecimal(result, base);
-    }
-    
-    private int calculate(String exp, String[] terms, int base) {
-        String[] first = terms[0].split("");
-        String[] second = terms[1].split("");
-        
         if (exp.contains("+")) {
-            return toDecimal(first, base) + toDecimal(second, base);
+            return toDecimal(first, base) + toDecimal(second, base) == toDecimal(result, base);
         } else {
-            return toDecimal(first, base) - toDecimal(second, base);
+            return toDecimal(first, base) - toDecimal(second, base) == toDecimal(result, base);
         }
     }
     
     private int toDecimal(String[] arr, int base) {
-        int result = 0;
-        int len = arr.length;
+        int result = 0, len = arr.length;
         for (int i = len - 1; i > -1; i--) {
-            result += Integer.parseInt(arr[i]) * Math.pow(base, len - 1 - i);
+            result += Integer.parseInt(arr[i]) * (int) Math.pow(base, len - 1 - i);
         }
-        return result;
+        return result;    
     }
     
-    private int toBase(int n, int base) {
-        if (n == 0) return 0;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        while (n > 0) {
-            int remainder = n % base;
-            sb.append(remainder);
-            n /= base;
-        }
-        
-        return Integer.parseInt(sb.reverse().toString());
+    private String[] parseExp(String exp) {
+        // 공백 치환
+        exp = exp.replace(" ", "");
+        // +-=를 치환
+        exp = exp.replace("+", ",")
+            .replace("-", ",")
+            .replace("=", ",");
+        return exp.split(",");
+    }
+    
+    
+    private String wrapAndReplace(String org, String del) {
+        if (org == null) return null;
+        return "\"" + org.replace("X", del) + "\"";
     }
     
     private void sout(Object obj) {
