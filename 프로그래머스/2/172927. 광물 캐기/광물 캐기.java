25fefa442680d3f971ac79eee22d78e2cdb33f88
sani <55998816+kimtks456@users.noreply.github.com>
2026-02-22 01:47:26 +0900

    Refactor mineral mining logic and fatigue calculation

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -3,13 +3,19 @@ import java.util.*;
 
 class Solution {
     
+    static Map<String, Integer> map = new HashMap<>() {{
+       put("diamond", 0);
+       put("iron", 1);
+       put("stone", 2);
+    }};
+    
     int result = Integer.MAX_VALUE;
     int n;
     
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
         n = minerals.length;
-        bt(picks, minerals, 0, 0, "", 0);
+        bt(picks, minerals, 0, 0, -1, 0);
         return result;
     }
     
@@ -17,49 +23,36 @@ class Solution {
                     String[] minerals, 
                     int idx, 
                     int fatigue,
-                    String nowTool,
+                    int nowTool,
                     int rest) {
-        // sout(picks);
-        // if (idx < n) 
-        // sout(new Object[] {idx, minerals[idx], fatigue, nowTool, rest, result});
         if (idx == n || 
             (Arrays.equals(picks, new int[] {0, 0, 0}) && rest == 0)) {
             result = Math.min(fatigue, result);
             return;
         }
         
-        int nowFatigue = getFatigue(nowTool, minerals[idx]);
+        int nowFatigue = getFatigue(nowTool, map.get(minerals[idx]));
         if (rest > 0) {
             bt(picks, minerals, idx + 1, fatigue + nowFatigue, nowTool, rest - 1);
         } else {
-            if (picks[0] > 0) {
-                picks[0] -= 1;
-            	bt(picks, minerals, idx, fatigue, "diamond", 5);
-                picks[0] += 1;
-            }
-            
-            if (picks[1] > 0) {
-                picks[1] -= 1;
-            	bt(picks, minerals, idx, fatigue, "iron", 5);
-                picks[1] += 1;
-            }
-            
-            if (picks[2] > 0) {
-                picks[2] -= 1;
-            	bt(picks, minerals, idx, fatigue, "stone", 5);
-                picks[2] += 1;
+            for (int i = 0; i < 3; i++) {
+                if (picks[i] > 0) {
+                    picks[i] -= 1;
+                	bt(picks, minerals, idx, fatigue, i, 5);    
+                    picks[i] += 1;
+                }
             }
         }
     }
     
-    private int getFatigue(String tool, String mineral) {
+    private int getFatigue(int tool, int mineral) {
         switch (tool) {
-            case "iron":
-                if (mineral.equals("diamond")) return 5;
+            case 1:
+                if (mineral == 0) return 5;
                 return 1;
-            case "stone":
-                if (mineral.equals("diamond")) return 25;
-                else if (mineral.equals("iron")) return 5;
+            case 2:
+                if (mineral == 0) return 25;
+                else if (mineral == 1) return 5;
                 return 1;
             default:
                 return 1;
