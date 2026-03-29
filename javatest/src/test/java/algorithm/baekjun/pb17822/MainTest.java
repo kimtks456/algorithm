e2dfe02d049df33a94a019e3c901d2c9d16ca7ab
kimtks456 <kimtks456@gmail.com>
2026-03-29 12:02:45 +0900

    feat: 백준 17822 풀이, 테스트 추가

---
+++ javatest/src/test/java/algorithm/baekjun/pb17822/MainTest.java
@@ -0,0 +1,94 @@
+package algorithm.baekjun.pb17822;
+
+import algorithm.baekjun.support.BaekjunTestBase;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+class MainTest extends BaekjunTestBase {
+
+    @Override
+    public void solve() throws IOException {
+        new Main().solve();
+    }
+
+    @Override
+    public Stream<Arguments> scenarioProvider() {
+        return Stream.of(
+                Arguments.of(
+                        """
+                                4 4 1
+                                1 1 2 3
+                                5 2 4 2
+                                3 1 3 5
+                                2 1 3 2
+                                2 0 1
+                                """,
+                        """
+                                30
+                                """
+                ),
+                Arguments.of(
+                        """
+                                4 4 2
+                                1 1 2 3
+                                5 2 4 2
+                                3 1 3 5
+                                2 1 3 2
+                                2 0 1
+                                3 1 3
+                                """,
+                        """
+                                22
+                                """
+                ),
+                Arguments.of(
+                        """
+                                4 4 3
+                                1 1 2 3
+                                5 2 4 2
+                                3 1 3 5
+                                2 1 3 2
+                                2 0 1
+                                3 1 3
+                                2 0 2
+                                """,
+                        """
+                                22
+                                """
+                ),
+                Arguments.of(
+                        """
+                                4 4 4
+                                1 1 2 3
+                                5 2 4 2
+                                3 1 3 5
+                                2 1 3 2
+                                2 0 1
+                                3 1 3
+                                2 0 2
+                                3 1 1
+                                """,
+                        """
+                                0
+                                """
+                ),
+                Arguments.of(
+                        """
+                                4 6 3
+                                1 2 3 4 5 6
+                                2 3 4 5 6 7
+                                3 4 5 6 7 8
+                                4 5 6 7 8 9
+                                2 1 4
+                                3 0 1
+                                2 1 2
+                                """,
+                        """
+                                26
+                                """
+                )
+        );
+    }
+}
