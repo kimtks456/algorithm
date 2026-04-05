ffa57d2d91e4cfc5da4db894eecb8f60b0da223b
sani <55998816+kimtks456@users.noreply.github.com>
2026-04-06 02:42:12 +0900

    [level 2] Title: 프로세스, Time: 0.05 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 4.11 MB, 시간: 0.01 ms
+메모리: 4.12 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 0:34:49
+2026년 04월 06일 02:42:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -6,37 +6,75 @@
 
 using namespace std;
 
-struct cmp {
-    bool operator()(int a, int b) {
-        return a < b;
-    }    
+struct Item {
+    int seq;
+    int priority;
+    
+    bool operator<(const Item& other) const {
+        if (priority != other.priority) return priority < other.priority;
+        return seq < other.seq;
+    }
 };
 
 int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    queue<pair<int, int>> q;
-    priority_queue<int, vector<int>, cmp> pq;
+    priority_queue<int> pq;
+    queue<vector<int>> que;
+    int answer = 1;
     
-    for (int i = 0; i < priorities.size(); i++) {
-        q.push({i, priorities[i]});
+    for (int i = 0; i < (int)priorities.size(); i++) {
         pq.push(priorities[i]);
+        que.push({i, priorities[i]});
     }
     
-    pair<int, int> temp;
-    while (!q.empty()) {
-        temp = q.front();
-        q.pop();
+    while (!pq.empty()) {
+        int item = pq.top();
+        vector<int> proc = que.front();
+        que.pop();
         
-        if (temp.second < pq.top()) {
-            q.push(temp);
+        if (item > proc[1]) {
+            que.push(proc);
+            continue;
         } else {
-            pq.pop();
+            if (proc[0] == location) return answer;
             answer++;
-            if (temp.first == location) {
-                break;
-            }
+            pq.pop();
         }
     }
     
     return answer;
 }
+
+// struct cmp {
+//     bool operator()(int a, int b) {
+//         return a < b;
+//     }    
+// };
+
+// int solution(vector<int> priorities, int location) {
+//     int answer = 0;
+//     queue<pair<int, int>> q;
+//     priority_queue<int, vector<int>, cmp> pq;
+    
+//     for (int i = 0; i < priorities.size(); i++) {
+//         q.push({i, priorities[i]});
+//         pq.push(priorities[i]);
+//     }
+    
+//     pair<int, int> temp;
+//     while (!q.empty()) {
+//         temp = q.front();
+//         q.pop();
+        
+//         if (temp.second < pq.top()) {
+//             q.push(temp);
+//         } else {
+//             pq.pop();
+//             answer++;
+//             if (temp.first == location) {
+//                 break;
+//             }
+//         }
+//     }
+    
+//     return answer;
+// }
