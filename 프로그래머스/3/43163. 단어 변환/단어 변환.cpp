a46cf7d1e84ff0ca4e5fbe8e3fac47ab6e253e16
sani <55998816+kimtks456@users.noreply.github.com>
2026-04-12 01:36:31 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,10 +1,11 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-int answer = 0;
+int answer = 50;
 
 // 1개만 차이나는지 비교
 bool isValid(const string& a, const string& b) {
@@ -19,16 +20,16 @@ void dfs(string cur,
          string target, 
          int dist,
          vector<string>& words, 
-         unordered_set<string> visited) {
+         unordered_set<string>& visited) {
     if (cur == target) {
-        answer = dist;
+        answer = min(dist, answer);
     }
     
     for (const string& str : words) {
         if (isValid(cur, str) && visited.count(str) == 0) {
             visited.insert(str);
             dfs(str, target, dist + 1, words, visited);
-            // visited.erase(str);
+            visited.erase(str);
         }
     }
 }
@@ -37,5 +38,6 @@ int solution(string begin, string target, vector<string> words) {
     unordered_set<string> visited;
     visited.insert(begin);
     dfs(begin, target, 0, words, visited);
+    if (answer == 50) answer = 0;
     return answer;
 }
